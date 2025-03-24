4b665c0ded17bd904e5b1c224ffdb90f4e927311
WalysonGomes <gomeswalyson258@gmail.com>
2025-03-24 13:08:12 -0300

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,55 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local harpoon = require("harpoon")
+
+-- The primeagen
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "=ap", "ma=ap'a")
+keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
 keymap.set("i", "jj", "<ESC>", { silent = true })
+
+keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+keymap.set("n", "<C-m>", function()
+  harpoon:list():select(1)
+end)
+keymap.set("n", "<C-n>", function()
+  harpoon:list():select(2)
+end)
+keymap.set("n", "<C-b>", function()
+  harpoon:list():select(3)
+end)
+keymap.set("n", "<C-t>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+keymap.set("n", "<C-p>", function()
+  harpoon:list():prev()
+end)
+keymap.set("n", "<C-o>", function()
+  harpoon:list():next()
+end)
+
 -- Do things without affecting the registers
 keymap.set("n", "x", '"_x')
 keymap.set("n", "<Leader>p", '"0p')
